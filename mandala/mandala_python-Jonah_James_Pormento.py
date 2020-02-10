7db13e8d05a2ed21ced87bdca9e22193472d3664
jjpormento21 <59875914+jjpormento21@users.noreply.github.com>
2020-02-10 09:25:45 +0800

    Add files via upload

---
+++ mandala/mandala_python-Jonah_James_Pormento.py
@@ -0,0 +1,21 @@
+import turtle
+
+window=turtle.Screen()
+window.bgcolor("#ffffff")
+m = turtle.Turtle()
+m.width(8)
+m.color("red")
+m.up()
+m.goto(0, 100)
+m.down()
+m.right(24)
+m.forward(40)
+for i in range(30):
+        m.right(47)
+        m.forward(50)
+        if range(25):
+                m.right(118)
+                m.forward(200)
+m.up()
+m.goto(150,0)
+turtle.done()
